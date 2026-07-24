b937cb81f807541b38d06179451cb1f0458175ff
Rahma Ashraf <rahmaashraf.dev@gmail.com>
2026-07-24 19:16:28 +0300

    modify cubit

--- lib/features/popular_tarining/presentation/view_model/popular_training_cubit.dart
+++ lib/features/popular_tarining/presentation/view_model/popular_training_cubit.dart
@@ -1,10 +1,29 @@
+import 'package:fitness/features/exercise/domain/use_cases/get_difficulty_levels_use_case.dart';
+import 'package:fitness/features/exercise/domain/use_cases/get_exercises_use_case.dart';
+import 'package:fitness/features/popular_tarining/domain/use_cases/get_random_exercise_use_case.dart';
+import 'package:fitness/features/popular_tarining/presentation/view_model/popular_training_intents.dart';
 import 'package:fitness/features/popular_tarining/presentation/view_model/popular_training_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class PopularTrainingCubit extends Cubit<PopularTrainingStates> {
-  PopularTrainingCubit() : super(const PopularTrainingStates());
+  final GetRandomExerciseUseCase _getRandomExerciseUseCase;
+  final GetExercisesUseCase _getExercisesUseCase;
+  final GetDifficultyLevelsUseCase _getDifficultyLevelsUseCase;
+  PopularTrainingCubit(
+    this._getRandomExerciseUseCase,
+    this._getExercisesUseCase,
+    this._getDifficultyLevelsUseCase,
+  ) : super(const PopularTrainingStates());
+
+  void doIntents(PopularTrainingIntents intent) {
+    switch (intent) {
+      case LoadPopularTrainingIntent():
+        _loadPopularTraining();
+        break;
+    }
+  }
 
   Future<void> _loadPopularTraining() async {
     emit(state.copyWith(isLoading: true));
